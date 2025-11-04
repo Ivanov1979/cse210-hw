96944ced0ff8be60e53f02d8d401b59d6c9ac69c
Ivanov1979 <ivansanhueza@yahoo.com>
2025-11-03 23:44:02 -0300

    Add files via upload

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage:");
+        string input = Console.ReadLine();
+        int percent = int.Parse(input);
+
+        string letter;
+
+        if (percent >= 90)
+            letter = "A";
+        else if (percent >= 80)
+            letter = "B";
+        else if (percent >= 70)
+            letter = "C";
+        else if (percent >= 60)
+            letter = "D";
+        else
+            letter = "F";
+            
+         Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (percent >= 70)
+            Console.WriteLine(" Congratulations! you passed the course.");
+
+        else Console.WriteLine("Keep trying - You can improve next time!");
     }
-}
+}   
+        
