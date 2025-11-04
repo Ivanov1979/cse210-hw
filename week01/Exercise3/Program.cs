8cc61b6c78989bd1b172bf1cec5042ec131908af
Ivanov1979 <ivansanhueza@yahoo.com>
2025-11-03 23:44:32 -0300

    Add files via upload

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // Generate a random number between 1 and 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("Welcome to 'Guess My Number'!");
+            Console.WriteLine("I have picked a number between 1 and 100.");
+
+            // Loop until user guesses correctly
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! The magic number was {magicNumber}.");
+                    Console.WriteLine($"You took {guessCount} guesses.");
+                }
+            }
+
+            // Ask user if they want to play again
+            Console.Write("\nDo you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
 }
